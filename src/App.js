e0fbb434f3fed5a2f34ddb30455f4421141e9d50
Huzaifa-code <huzaifaqureshi037@gmail.com>
2023-01-05 19:31:48 +0530

    portfolio v3.0.1 added custom cursor

--- src/App.js
+++ src/App.js
@@ -5,10 +5,41 @@ import Hero from './components/Hero/Hero';
 import Navbar from './components/NavBar/Navbar';
 import Projects from './components/Projects/Projects';
 import Skills from './components/Skills/Skills';
+import { useEffect } from "react";
+import { motion, useMotionValue, useSpring } from "framer-motion";
 
 function App() {
+
+  const cursorX = useMotionValue(-100);
+  const cursorY = useMotionValue(-100);
+
+  const springConfig = { damping: 25, stiffness: 700 };
+  const cursorXSpring = useSpring(cursorX, springConfig);
+  const cursorYSpring = useSpring(cursorY, springConfig);
+
+  useEffect(() => {
+    const moveCursor = (e) => {
+      cursorX.set(e.clientX - 16);
+      cursorY.set(e.clientY - 16);
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, [cursorX,cursorY ]);
+
   return (
-    <div>
+    <div className='bg-white'>
+
+      <motion.div
+          className="cursor"
+          style={{
+            translateX: cursorXSpring,
+            translateY: cursorYSpring,
+          }}
+      />
       
       <Navbar/>
       <Hero/>
