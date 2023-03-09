6edce00b0eaba3bd7a92d0493c714d4d1e4e4c5f
Huzaifa-code <huzaifaqureshi037@gmail.com>
2023-03-09 17:52:59 +0530

    fixed button hover and progress bar smooth

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,18 +7,23 @@ import Navbar from '../components/NavBar/Navbar';
 import Projects from '../components/Projects/Projects';
 import Skills from '../components/Skills/Skills';
 
-import { motion, useScroll } from "framer-motion"
+import { motion, useScroll,useSpring } from "framer-motion"
 
 
 
 const Home = () => {
 
     const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress, {
+      stiffness: 100,
+      damping: 25,
+      restDelta: 0.001
+    });
    
     return (
       <div className='bg-white'>
         <Navbar/>
-        <motion.div className='fixed top-0 left-0 right-0 h-[10px] bg-[#731FFC] origin-top-left' style={{ scaleX: scrollYProgress }} />  
+        <motion.div className='fixed top-0 left-0 right-0 h-[10px] bg-[#731FFC] origin-top-left' style={{ scaleX }} />  
         <Hero/>
         <About/>
         <Skills/>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -141,7 +141,7 @@ function Hero() {
                 <div className="text flex flex-col items-center">
                   <h1 className='text-center font-semibold text-3xl md:text-4xl' data-aos="fade-up" data-aos-duration="1000" >Hi , I am</h1>
                   <h1 className='text-center font-bold text-4xl md:text-5xl md:mt-2' data-aos="fade-up" data-aos-duration="1000" >Huzaifa Qureshi</h1> 
-                  <p className='text-center font-regular text-xl px-12 md:px-[26rem] md:text-2xl md:mt-2' data-aos="fade-up" data-aos-duration="1000" >I help Businesses <span className='font-bold text-[#2ed573]'>GROW</span>  and <span className='font-bold text-[#2ed573]'>INCREASE</span> their social presence by building intuitive Websites</p> 
+                  <p className='text-center font-regular text-xl px-12 md:px-[26rem] md:text-2xl md:mt-2' data-aos="fade-up" data-aos-duration="1000" >I help Businesses <span className='font-bold'>GROW</span>  and <span className='font-bold'>INCREASE</span> their social presence by building intuitive Websites</p> 
                   <Link smooth spy to="contact" className='my-5 py-3 w-[10rem]  flex items-center justify-center bg-[#731FFC] font-semibold text-xl text-white rounded-md hover:scale-105'>Hire Me <img className='ml-4' src={profileimg} alt="" /> </Link>         
                 </div>
               </div> 
@@ -155,9 +155,9 @@ function Hero() {
         </div>
       
         <div className='fixed bg-[#fff] md:bg-transparent left-[83vw] md:left-auto md:right-10 mt-[10vh] md:mt-[26vh] pl-3'>
-          <SocialBtn className='' target="_blank" href="https://www.instagram.com/dev_huzaifa"> <div className='p-3 my-3 rounded-full border border-solid border-black hover:rounded-xl '> <img className=''  src={insta} alt="" /></div> </SocialBtn>
-          <SocialBtn className='' target="_blank" href="https://www.linkedin.com/in/huzaifa-qureshi-174173179"> <div className='p-3 rounded-full border border-solid border-black hover:rounded-xl'><img src={lin} alt="" /></div>  </SocialBtn>
-          <SocialBtn className='' target="_blank" href="https://github.com/Huzaifa-code"> <div className='p-3 my-3 rounded-full border border-solid border-black hover:rounded-xl'><img src={gh} alt="" /></div>  </SocialBtn>
+          <SocialBtn className='' target="_blank" href="https://www.instagram.com/dev_huzaifa"> <div className='p-3 my-3 rounded-full border border-solid border-black hover:scale-110'> <img  src={insta} alt="" /></div> </SocialBtn>
+          <SocialBtn className='' target="_blank" href="https://www.linkedin.com/in/huzaifa-qureshi-174173179"> <div className='p-3 rounded-full border border-solid border-black hover:scale-110'><img src={lin} alt="" /></div>  </SocialBtn>
+          <SocialBtn className='' target="_blank" href="https://github.com/Huzaifa-code"> <div className='p-3 my-3 rounded-full border border-solid border-black hover:scale-110'><img src={gh} alt="" /></div>  </SocialBtn>
         </div>
     </div>
   )
