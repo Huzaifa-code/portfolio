de202866f5f44769589003258d34f7e7e8c45641
Huzaifa-code <huzaifaqureshi037@gmail.com>
2023-04-18 13:13:41 +0530

    made skills image carousel

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -8,15 +8,21 @@ import nodejs from './assets/nodejs.svg'
 import react from './assets/react.svg'
 import ts from './assets/ts.svg'
 
+//for carousel
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+import '@splidejs/react-splide/css';
+
 
 function Skills() {
 
+  const images = [ react, nodejs, ts, mongodb, js, java, cplus, figma ];
     
   return (
     <div id='skills'>
         <h1 className='text-4xl md:text-5xl md:ml-11 my-4 md:my-16 font-bold text-center'>MY TECH SKILLS</h1>
         
-        <div className='md:mx-24 '>
+        {/* <div className='md:mx-24 '>
             <div className='flex items-center justify-center'>
                 <img className='h-[5.5rem] md:h-[8rem] mx-4 md:mx-24' src={react} alt="" />
                 <img className='h-[5.5rem] md:h-[8rem] mx-4 md:mx-24' src={nodejs} alt="" />
@@ -38,7 +44,47 @@ function Skills() {
               <img className='h-[5.5rem] md:h-[8rem]' src={figma} alt="" />
             </div>
         </div>
-        
+         */}
+
+
+        <Splide className=' py-6'
+          options={ {
+            gap   : '5px',
+            perPage: 5,
+            type: 'loop',
+            autoScroll: {
+              pauseOnHover: false,
+              pauseOnFocus: false,
+              rewind: false,
+              speed: 3
+            },
+            arrows: false,
+            pagination: false,
+            breakpoints: {
+                600: {
+                    perPage: 2,
+                } 
+            }
+              } } 
+            aria-label="My Favorite Images"
+            extensions={{ AutoScroll }}
+          >
+
+            {
+              images.map( (image, key) => {
+                return (
+                  <SplideSlide key={key} className=''>
+                    <div className='py-9' >
+                      <img className='md:h-[10rem] h-[7rem]' src={image} alt="" />
+                    </div>
+                  </SplideSlide>
+                )
+              } )
+            }
+              
+        </Splide>            
+
+
     </div>
   )
 }
