8a22eeb2be629f355cbdd5cc13dab10561f9e6cc
Huzaifa-code <huzaifaqureshi037@gmail.com>
2024-06-08 16:03:03 +0530

    SEO - HELMET-REACT

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Connect from './Pages/Connect.jsx';
 import Login from './Pages/Login.jsx';
 import Signup from './Pages/Signup.jsx';
 import Projects from './Pages/Projects.jsx';
+import { Helmet } from 'react-helmet';
 
 
 function App() {
@@ -15,7 +16,20 @@ function App() {
   return (
     // <BrowserRouter>
     <>
-      
+      <Helmet>
+        <title>Huzaifa Qureshi - Software Engineer</title>
+        <meta name="description" content="Huzaifa Qureshi is a FullStack web and app developer. Highly Skilled in - MERN, React.js, React-Native, Next.js, Node.js, MongoDb, Tailwind css, firebase, sanity.io"/>
+        <meta name="keywords" content="Software Engineer ,MERN web developer - Huzaifa Qureshi"/>
+        <meta name="author" content="Huzaifa Qureshi"/>
+
+        {/* <!-- Geo Meta Tags --> */}
+        <meta name="geo.region" content="IN-MP"/>
+        <meta name="geo.placename" content="Indore"/>
+        {/* <meta name="geo.position" content="22.74454918810034, 75.79635383561423"/> 
+        <meta name="ICBM" content="22.74454918810034, 75.79635383561423"/> */}
+
+      </Helmet>
+
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/connect' element={<Connect/>} />
