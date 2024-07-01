f06109895b3bf5ca2817b03d2fc94ef8bf91c95c
Huzaifa-code <huzaifaqureshi037@gmail.com>
2024-07-01 14:59:49 +0530

    FEAT: added category tags

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -26,6 +26,10 @@ const Blog = () => {
                     url
                 },
                 alt
+            },
+            categories[]->{
+              _id,
+              title
             }
         }`
     ).then((data) => { 
@@ -62,11 +66,21 @@ const Blog = () => {
                   style={{ y: 30 }} animate={{ y: 0 }} 
                   transition={{duration: 0.55}}
                   key={post.slug.current}
-                  
                 >
                   <article className='flex flex-col items-start justify-between h-full' >
                       <img className='rounded-xl md:w-full' src={post.mainImage.asset.url} alt={post.title} />
-                      <p className='text-sm font-medium mt-2 text-neutral-500'>{formatDate(post.publishedAt)}</p>
+                      <div className='flex justify-between items-center w-full mt-2'>
+                        <p className='text-sm font-medium  text-neutral-500'>{formatDate(post.publishedAt)}</p>
+                        <div>
+                          {post.categories?.map((category) => (
+                            category ? (
+                              <span key={category._id} className='mr-2 bg-yellow-200 px-3 py-1 rounded-md border border-yellow-400 text-yellow-400'>
+                                {category.title}
+                              </span>
+                            ) : null
+                          ))}
+                        </div>
+                      </div>
                       <h4 className='text-xl mt-2'>{post.title}</h4>
                       <button className='mt-5 mb-10'>
                           <Link to={`/blog/${post.slug.current}`}  className='py-2 px-6 rounded shadow text-white bg-black hover:bg-transparent border-2 border-black transition-all duration-500 hover:text-black font-bold' >Read Full Article</Link>
