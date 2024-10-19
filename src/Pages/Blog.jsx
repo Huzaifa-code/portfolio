0445e9615f6db5be1b7d643461e98ee1995674e1
Huzaifa-code <huzaifaqureshi037@gmail.com>
2024-10-19 14:26:10 +0530

    feat: added loader

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,41 +1,44 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+// Blog.js
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import client from '../client'
-import {NavBarBlog} from '../components/Blog';
+import client from '../client';
+import { NavBarBlog } from '../components/Blog';
 import { motion } from 'framer-motion';
-
+import Loader from '../components/Loader/Loader'; // Import the Loader component
 
 const Blog = () => {
-
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true); // Loading state
 
   useEffect(() => {
-
     client.fetch(
-        `*[_type == "post"] | order(publishedAt desc) {
-            title,
-            slug,
-            body,
-            publishedAt,
-            mainImage {
-                asset -> {
-                    _id,
-                    url
-                },
-                alt
-            },
-            categories[]->{
-              _id,
-              title
-            }
-        }`
-    ).then((data) => { 
-        setPosts(data) 
-    }).catch(
-        console.error()
+      `*[_type == "post"] | order(publishedAt desc) {
+        title,
+        slug,
+        body,
+        publishedAt,
+        mainImage {
+          asset -> {
+            _id,
+            url
+          },
+          alt
+        },
+        categories[]->{
+          _id,
+          title
+        }
+      }`
     )
-  }, [])
+    .then((data) => {
+      setPosts(data);
+      setLoading(false); // Set loading to false once data is fetched
+    })
+    .catch((error) => {
+      console.error(error);
+      setLoading(false); // In case of error, also stop loading
+    });
+  }, []);
 
   const formatDate = (isoString) => {
     const date = new Date(isoString);
@@ -46,53 +49,56 @@ const Blog = () => {
     });
   };
 
- 
   return (
-    <div className='bg-white' >
-        <NavBarBlog/>
-        <section className='px-10'>
-          <motion.div 
-            style={{ y: 30 }} animate={{ y: 0 }}
-            transition={{duration: 0.5}}
-          >
-            <h1 className='font-bold text-4xl md:text-5xl pt-5 ' >Welcome to My Dev Blog</h1>
-            <p className="my-11 text-lg md:text-xl lg:text-xl text-neutral-600 max-w-4xl">
-              Join me as I delve into the fascinating world of software engineering! Explore a wealth of articles on invaluable Linux tips and tricks,  React.js and cutting-edge web technologies . Whether you're just starting out or you're a seasoned professional, you'll find insightful content and practical guides to elevate your skills and knowledge.
-            </p>
-          </motion.div>
+    <div className='bg-white'>
+      <NavBarBlog/>
+      <section className='px-10'>
+        <motion.div 
+          style={{ y: 30 }} animate={{ y: 0 }}
+          transition={{duration: 0.5}}
+        >
+          <h1 className='font-bold text-4xl md:text-5xl pt-5'>Welcome to My Dev Blog</h1>
+          <p className="my-11 text-lg md:text-xl lg:text-xl text-neutral-600 max-w-4xl">
+            Join me as I delve into the fascinating world of software engineering! Explore a wealth of articles on invaluable Linux tips and tricks, React.js and cutting-edge web technologies.
+          </p>
+        </motion.div>
 
+        {loading ? ( 
+          <Loader /> // Show loader when posts are being fetched
+        ) : (
           <div className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 lg:gap-24 mb-11'>
             {posts.map((post) => (
-                <motion.div
-                  style={{ y: 30 }} animate={{ y: 0 }} 
-                  transition={{duration: 0.55}}
-                  key={post.slug.current}
-                >
-                  <Link to={`/blog/${post.slug.current}`}  className='flex flex-col items-start justify-between h-full border-2 rounded-2xl border-black-400' >
-                      <img className='rounded-xl md:w-full' src={post.mainImage.asset.url} alt={post.title} />
-                      <div className='w-full mt-2 px-4'>
-                        <p className='text-sm font-medium  text-neutral-500'>{formatDate(post.publishedAt)}</p>
-                        <div className='space-x-2'>
-                          {post.categories?.map((category) => (
-                            category ? (
-                              <span key={category._id} className=' bg-purple-200 px-3 py-1 rounded-md text-sm text-purple-400'>
-                                {category.title}
-                              </span>
-                            ) : null
-                          ))}
-                        </div>
-                      </div>
-                      <h4 className='text-md font-medium mt-2 px-4'>{post.title}</h4>
-                      <button className='mt-5 mb-8 mx-4 w-11/12'>
-                          <p  className='py-2 px-6 rounded-xl shadow text-white bg-black hover:bg-transparent border-2 border-black transition-all duration-500 hover:text-black font-bold' >Read Full Article</p>
-                      </button>
-                  </Link>
-                </motion.div>
+              <motion.div
+                style={{ y: 30 }} animate={{ y: 0 }} 
+                transition={{duration: 0.55}}
+                key={post.slug.current}
+              >
+                <Link to={`/blog/${post.slug.current}`} className='flex flex-col items-start justify-between h-full border-2 rounded-2xl border-black-400'>
+                  <img className='rounded-xl md:w-full' src={post.mainImage.asset.url} alt={post.title} />
+                  <div className='w-full mt-2 px-4'>
+                    <p className='text-sm font-medium text-neutral-500'>{formatDate(post.publishedAt)}</p>
+                    <div className='space-x-2'>
+                      {post.categories?.map((category) => (
+                        category ? (
+                          <span key={category._id} className='bg-purple-200 px-3 py-1 rounded-md text-sm text-purple-400'>
+                            {category.title}
+                          </span>
+                        ) : null
+                      ))}
+                    </div>
+                  </div>
+                  <h4 className='text-md font-medium mt-2 px-4'>{post.title}</h4>
+                  <button className='mt-5 mb-8 mx-4 w-11/12'>
+                    <p className='py-2 px-6 rounded-xl shadow text-white bg-black hover:bg-transparent border-2 border-black transition-all duration-500 hover:text-black font-bold'>Read Full Article</p>
+                  </button>
+                </Link>
+              </motion.div>
             ))}
           </div>
-        </section>
+        )}
+      </section>
     </div>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+// Loader.js
+import React from 'react';
+import './Loader.css'; // Importing the loader styles
+
+const Loader = () => {
+  return (
+    <div className="loader-modal">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
