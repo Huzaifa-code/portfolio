adf3c22c848c62739d05e062b87e6949d3560f8f
Huzaifa-code <huzaifaqureshi037@gmail.com>
2024-10-21 12:16:50 +0530

    fix: z-index of filter

--- src/components/Blog/Filter.jsx
+++ src/components/Blog/Filter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Filter({ categories, activeFilter, setActiveFilter }) {
   return (
-    <div className="w-full overflow-x-auto py-3 px-10 sticky bg-white top-0 ">
+    <div className="w-full overflow-x-auto py-3 px-3 md:px-10 sticky bg-transparent backdrop-blur-md top-0 z-1 ">
       <div className="flex space-x-4">
         {categories?.map((category, index) => (
           // Check if category is not null or undefined before rendering
